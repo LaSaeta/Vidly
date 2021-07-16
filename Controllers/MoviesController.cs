dc79777a910d853582bf26d74aa2d51a87c34899
Satya <satyap48@gmail.com>
2021-07-16 12:35:04 +0530

    Added Movies and their details in the view

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,24 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
-using Vidly.Models;
+using Vidly.Data;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private DataContext _dataContext { get; set; }
+        public MoviesController()
+        {
+            _dataContext = new DataContext();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek!" },
-                new Movie { Id = 2, Name = "Wall-E" }
-            };
+            var movies = _dataContext.Movies
+                         .Include(m => m.Genre)
+                         .ToList();
+
 
             var viewModel = new MovieViewModel { Movies = movies };
 
             return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _dataContext.Movies
+                        .Include(m => m.Genre)
+                        .SingleOrDefault(m => m.Id == id);
+
+            return View(movie);
         }
     }
 }
